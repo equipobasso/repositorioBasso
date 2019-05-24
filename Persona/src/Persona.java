a7ac4142f331b03c3f90e26e088668af8cd547e8
abasso19 <alebasaba96@gmail.com>
2019-05-24 13:22:48 +0200

    Getters y Setters

--- Persona/src/Persona.java
+++ Persona/src/Persona.java
@@ -11,7 +11,30 @@ public class Persona {
 		this.edad = edad;
 		this.nacionalidad = nacionalidad;
 	}
-	
-	
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+	
+	
 }
