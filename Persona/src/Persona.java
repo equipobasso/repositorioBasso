d722f34514a6b3f7f60cb54b39a2c79334204b10
Manuel <manuel.rios.herrera.dam@gmail.com>
2019-05-24 13:35:18 +0200

    Agregacion de JavaDoc

--- Persona/src/Persona.java
+++ Persona/src/Persona.java
@@ -1,37 +1,67 @@
-
+/**
+ * 
+ * @author usuario
+ * Clase persona
+ */
 public class Persona {
-	
+	/**
+	 *Atributos de la clase persona
+	 */
 	private String nombre;
 	private int edad;
 	private String nacionalidad;
-	
+	/**
+	 * Constructor clase Persona
+	 * @param nombre
+	 * @param edad
+	 * @param nacionalidad
+	 */
 	public Persona(String nombre, int edad, String nacionalidad) {
 		super();
 		this.nombre = nombre;
 		this.edad = edad;
 		this.nacionalidad = nacionalidad;
 	}
-
+	/**
+	 * Obtenemos el nombre.
+	 * @return
+	 */
 	public String getNombre() {
 		return nombre;
 	}
-
+	/**
+	 * Asignamos un nombre
+	 * @param nombre
+	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	/**
+	 *Obtenemos la edads
+	 * @return
+	 */
 
 	public int getEdad() {
 		return edad;
 	}
-
+	/**
+	 * Asignamos la edad
+	 * @param edad
+	 */
 	public void setEdad(int edad) {
 		this.edad = edad;
 	}
-
+	/**
+	 * Obtenemos la nacionalidad
+	 * @return
+	 */
 	public String getNacionalidad() {
 		return nacionalidad;
 	}
-
+	/**
+	 * Asignamos la nacionalidad
+	 * @param nacionalidad
+	 */
 	public void setNacionalidad(String nacionalidad) {
 		this.nacionalidad = nacionalidad;
 	}
