2555f12f5139960793303b755ce6129ca852b801
abasso19 <alebasaba96@gmail.com>
2019-05-24 13:05:23 +0200

    Añadido el constructor

--- Persona/src/Persona.java
+++ Persona/src/Persona.java
@@ -1,7 +1,17 @@
 
 public class Persona {
+	
 	private String nombre;
 	private int edad;
 	private String nacionalidad;
+	
+	public Persona(String nombre, int edad, String nacionalidad) {
+		super();
+		this.nombre = nombre;
+		this.edad = edad;
+		this.nacionalidad = nacionalidad;
+	}
+	
+	
 
 }
